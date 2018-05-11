a37111434421f2ca50350ec3f985fb14a0783caf
Bryanna Brueggman <bryannabrueggman@MacBook-Pro.local>
2018-05-10 20:09:59 -0500

    commit

--- OOP/OOP.cs
+++ OOP/OOP.cs
@@ -1,73 +1,87 @@
 ﻿using System;
 
-namespace OOP
+public class Program
 {
-    class Program
-    {
-        static void Main()
-        {
-            Person rebel = new Person("Leia" , "Organa");
-            Person imperial = new Person("Darth" , "Vader");
-            Console.WriteLine("Hello World!");
-        }
-    }  
-    class Person
-    {
-        private string firstName;
-        private string lastName;
-        private string password;
-        public Person(string firstName, string lastName, string alliance)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.alliance = alliance;
-        }
-        public string FullName
-        { 
-            get {
-                return this.firstName + " " + this.lastName; 
-            }
-            set {
-                string[] names = value.Split(' ');                
-                this.firstName = names[0];
-                this.lastName = names[1];
-            }
-        } 
-        class Ship
-        {
-            private Person[] passengers;
-
-            public Ship(string alliance, string type, int size);
-            {
-                    this,type = type;
-                    this.allaince = alliance;
-                    this.passengers = new Person[size];
-            }
-            public string Type {get; set;}
-            public string Alliance {get; set;}
-
-            public string Passengers{
-                get{
-                    foreach (var person in passengers)
-                    {
-                        Console.WriteLine(String.Format("{0}", person.FullName));
-                    }
-                    return "That's Everybody!";
-                }
-            }
-            public void EnterShip(Person person, int seat) 
-            {
-                this.Passengers [seat]
-            } 
-            public void ExitShip(int seat)
-            {
-                this.Passengers [seat] 
-            }     
-
-            class Station
-            {
-
-            }
-    }
+	public static void Main()
+	{
+		Person leia = new Person("Leia", "Organa", "Rebel");
+		Person darth = new Person("Darth", "Vader", "Imperial");
+		Ship falcon = new Ship("Rebel", "Smuggling", 2);
+		Ship tie = new Ship("Tie", "Fighter", 1);
+		Console.WriteLine("Hello world!");
+	}
 }
-    
+
+class Person
+{
+	private string firstName;
+	private string lastName;
+	private string alliance;
+	public Person(string firstName, string lastName, string alliance)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.alliance = alliance;
+	}
+
+	public string FullName
+	{
+		get
+		{
+			return this.firstName + " " + this.lastName;
+		}
+
+		set
+		{
+			string[] names = value.Split(' ');
+			this.firstName = names[0];
+			this.lastName = names[1];
+		}
+	}
+}
+
+class Ship
+{
+	private Person[] passengers;
+	public Ship(string alliance, string type, int size)
+	{
+		this.Type = type;
+		this.Alliance = alliance;
+		this.passengers = new Person[size];
+	}
+
+	public string Type
+	{
+		get;
+		set;
+	}
+
+	public string Alliance
+	{
+		get;
+		set;
+	}
+
+	public string Passengers
+	{
+		get
+		{
+			foreach (var person in passengers)
+			{
+				Console.WriteLine(String.Format("{0}", person.FullName));
+			}
+
+			return "That's Everybody!";
+		}
+	}
+
+	public void EnterShip(Person person, int seat)
+	{
+		this.passengers[seat] = person;
+	}
+
+	public void ExitShip(int seat)
+	{
+		this.passengers[seat] = null;
+	}
+}
