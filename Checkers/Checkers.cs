445e039b97220c1efac842c199538af5cbd5b92c
Bryanna Brueggman <bryannabrueggman@MacBook-Pro.local>
2018-06-05 18:59:54 -0500

    checkers

--- Checkers/Checkers.cs
+++ Checkers/Checkers.cs
@@ -8,56 +8,74 @@ namespace Checkers
     {
         static void Main(string[] args)
         {
+            int[][] whitePositions = new int[][] {
+                new int[] { 0, 1 }, new int[] { 0, 3 }, new int[] { 0, 5 }, new int[] { 0, 7 },
+                new int[] { 1, 0 }, new int[] { 1, 2 }, new int[] { 1, 4 }, new int[] { 1, 6 },
+                new int[] { 2, 1 }, new int[] { 2, 3 }, new int[] { 2, 5 }, new int[] { 2, 7 }
+            };
             
-            Console.WriteLine("Hello World!");
+
+            int[][] blackPositions = new int[][] {
+                new int[] { 5, 0 }, new int[] { 5, 2 }, new int[] { 5, 4 }, new int[] { 5, 6 },
+                new int[] { 6, 1 }, new int[] { 6, 3 }, new int[] { 6, 5 }, new int[] { 6, 7 },
+                new int[] { 7, 0 }, new int[] { 7, 2 }, new int[] { 7, 4 }, new int[] { 7, 6 }
+            };
+
+            Board board = new Board();
+            
+            for (int i = 0; i < 12; i++)
+            {
+                Checker white = new Checker("white", whitePositions[i]);
+                Checker black = new Checker("black", blackPositions[i]);
+                board.Checkers.Add(white);
+                board.Checkers.Add(black);
+            }
+            board.DrawBoard();
+            Console.WriteLine("hello, world");
         }
     }
 
     public class Checker
     {
-        public string Symbol  
-        { 
-            get
-            {
-                return Symbol;
-            } 
-            set
-            {
-                Symbol = value;
-            } 
-        }
+        public string Symbol  { get; set; }
         public int[] Position  { get; set; }
         public string Color { get; set; }
         
         public Checker(string color, int[] position)
         {
-            Color = color; 
-            Position = position;
-            if (Color == "white")
+            int circleId;
+            if (color == "white")
             {
-                int openCircleId = int.Parse("25CB", System.Globalization.NumberStyles.HexNumber);
-                string openCircle = char.ConvertFromUtf32(openCircleId);
-                Symbol = openCircle;
+                circleId = int.Parse("25CB", System.Globalization.NumberStyles.HexNumber);
             }
             else
             {
-                int closedCircleId = int.Parse("25CF", System.Globalization.NumberStyles.HexNumber);
-                string closedCircle = char.ConvertFromUtf32(closedCircleId);
-                Symbol = closedCircle;
+                circleId = int.Parse("25CF", System.Globalization.NumberStyles.HexNumber);
             }
+            this.Symbol = char.ConvertFromUtf32(circleId);
+            this.Position = position;
         }
     }
 
     public class Board
     {
-        private List<Checker> checkers = new List<Checker>();
-        public static int[,] whitePosition = new int[12,2];
-        private static int[,] blackPosition = new int[12, 2];
-
-
+        public string[][] Grid  { get; set; }
+        public List<Checker> Checkers { get; set; }
+        
         public Board()
         {
-         
+            this.Checkers = new List<Checker>();
+            this.Grid = new string[][] { 
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+            };
+            
             return;
         }
         
@@ -75,19 +93,27 @@ namespace Checkers
         
         public void PlaceCheckers()
         {
-            // Your code here
+            foreach (var checker in Checkers)
+            {
+                this.Grid[checker.Position[0]][checker.Position[1]] = checker.Symbol;
+            }
             return;
         }
         
         public void DrawBoard()
         {
-            // Your code here
+            PlaceCheckers();
+            Console.WriteLine("  0 1 2 3 4 5 6 7");
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(i + " " + String.Join(" ", this.Grid[i]));
+            }
             return;
         }
         
         public Checker SelectChecker(int row, int column)
-        {
-            return Checkers.Find(x => x.Position.SequenceEqual(new List<int> { row, column }));
+       {
+           return Checkers.Find(x => x.Position.SequenceEqual(new List<int> { row, column }));
         }
         
         public void RemoveChecker(int row, int column)
@@ -98,7 +124,7 @@ namespace Checkers
         
         public bool CheckForWin()
         {
-            return Checkers.All(x => x.Color == "white") || !Checkers.Exists(x => x.Color == "white");
+           return Checkers.All(x => x.Color == "white") || !Checkers.Exists(x => x.Color == "white");
         }
     }
 
